8a18d8248e723690cff489563037434bbb7e7019
Dariusz Pankowski <darek.pankowski@poczta.fm>
2025-07-07 22:12:02 +0200

    dodanie autoryzacji allegro

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from auth import router as auth_router
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
