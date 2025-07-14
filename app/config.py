0293ec34121bfbd8104cb1b202a59bcb58e58cc1
Dariusz Pankowski <darek.pankowski@poczta.fm>
2025-07-14 10:30:44 +0200

    poprawa nazw zmiennych systemowych

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-REDIRECT_URI = os.getenv("REDIRECT_URI")
+CLIENT_ID = os.getenv("ALLEGRO_CLIENT_ID")
+CLIENT_SECRET = os.getenv("ALLEGRO_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("ALLEGRO_REDIRECT_URI")
 AUTH_URL = "https://allegro.pl/auth/oauth/authorize"
 TOKEN_URL = "https://allegro.pl/auth/oauth/token"
